e7faca0d477cd56e97c7c1e985a2f42750d24bed
Klaartje Claire <rosemarieclaire@gmail.com>
2020-09-06 11:14:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-object(Recipes);
 var Recipes = (soup.tomato);
 
 function updateObjectWithKeyAndValue(Recipes, soup, tomato){
 
 var newRecipes = Object.assign({}, soup);
-newRecipes(salad.cucomber);
 }
+newRecipes = (salad.carrot);
