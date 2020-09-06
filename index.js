472f2aed03974d51352eaf5cc5ab0eb629932d94
Klaartje Claire <rosemarieclaire@gmail.com>
2020-09-06 11:33:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var recipes = (soup.tomato);
 
+function updateObjectWithKeyAndValue(){
 var newRecipes = Object.assign({}, soup);
+}
 
 newRecipes = (salad.carrot);
 
 function destructivelyUpdateObjectWithKeyValue(recipes, soup, tomato){
 recipes(salad, carrot);
+}
+function deleteFromObjectByKey(object, key){
+    delete newRecipes.soup; 
+}
+
+function destructivelyDeleteFromObjectByKey(){
+  delete recipes.soup;
 }
